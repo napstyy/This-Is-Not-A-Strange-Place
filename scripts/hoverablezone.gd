2b0eec01a66960ad73d30636eb972de05ffa9817
doubleTS <tylertsoberanis@gmail.com>
2026-02-17 19:45:35 -0600

    partial level transitions for hoverable zones

--- scripts/hoverablezone.gd
+++ scripts/hoverablezone.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 @export var mouseCursor: AnimatedSprite2D
 @export var hoverCursor: String #change depending on which cursor is needed
+@export var level: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,10 +16,13 @@ func _process(delta: float) -> void:
 #on mouse hover, change sprite to hoverCursor
 func _on_mouse_entered() -> void:
 	mouseCursor.play(hoverCursor)
-	#print("entered")
 
-
+#return mouse cursor to default
 func _on_mouse_exited() -> void:
 	mouseCursor.play("default")
-	#print("exit")
-	
+
+#change level on click
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file(level)
+		print("clicked")
