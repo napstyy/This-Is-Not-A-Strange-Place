9bd52f41bed59d44e8a66d59044f2c31543c9e1f
doubleTS <tylertsoberanis@gmail.com>
2026-02-17 19:32:16 -0600

    cursor working

--- scripts/custom_cursor.gd
+++ scripts/custom_cursor.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#sets custom cursor for mouse
 	Input.set_custom_mouse_cursor(sprite_frames.get_frame_texture(animation, frame), Input.CURSOR_ARROW, Vector2(sprite_frames.get_frame_texture(animation, frame).get_width(), sprite_frames.get_frame_texture(animation, frame).get_height()) / 2)
--- scripts/hoverablezone.gd
+++ scripts/hoverablezone.gd
@@ -1,25 +1,24 @@
 extends Area2D
 
-@export var hoverSprite: AnimatedSprite2D
-@export var defaultSprite: AnimatedSprite2D
-var defaultframes
+
+@export var mouseCursor: AnimatedSprite2D
+@export var hoverCursor: String #change depending on which cursor is needed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	defaultframes = defaultSprite.sprite_frames
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-#on mouse hover, change sprite to mouseSprite
+#on mouse hover, change sprite to hoverCursor
 func _on_mouse_entered() -> void:
-	defaultSprite.sprite_frames = hoverSprite.sprite_frames
-	print("entered")
+	mouseCursor.play(hoverCursor)
+	#print("entered")
 
 
 func _on_mouse_exited() -> void:
-	defaultSprite.sprite_frames = defaultframes
+	mouseCursor.play("default")
+	#print("exit")
 	
