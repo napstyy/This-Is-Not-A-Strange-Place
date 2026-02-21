61b187d9ea3d4f9031a31c49536c115ee573b7d5
napstyy <omajidi5001@gmail.com>
2026-02-21 03:31:24 -0500

    unpicked up the torch

--- scripts/Torch.gd
+++ scripts/Torch.gd
@@ -1,68 +1,24 @@
 extends RigidBody2D
 
-var dragging: bool = false
 var attached_to: CharacterBody2D = null
 var attach_offset: Vector2 = Vector2.ZERO
 var original_collision_layer: int = 0
 var original_collision_mask: int = 0
 
-@onready var attach_area: Area2D = $AttachArea
-
 @export var prefab_id := 8
 
 func _ready() -> void:
 	add_to_group("torch")
-
 	original_collision_layer = collision_layer
 	original_collision_mask = collision_mask
-	if attach_area:
-		attach_area.monitoring = true
-		attach_area.input_pickable = true
-		attach_area.connect("input_event", Callable(self, "_on_attach_area_input"))
-
-func _on_attach_area_input(viewport, event, shape_idx) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			if attached_to:
-				_detach()
-			dragging = true
-		else:
-			dragging = false
-			_try_attach_on_drop()
-
-func _input_event(viewport, event, shape_idx) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			if attached_to:
-				_detach()
-			dragging = true
-		else:
-			print("hello")
-			dragging = false
-			_try_attach_on_drop()
 
 func _physics_process(delta: float) -> void:
-	if dragging:
-		linear_velocity = Vector2.ZERO
-		global_position = get_global_mouse_position()
-		return
 	if attached_to:
 		if not attached_to.is_inside_tree():
 			_detach()
 		else:
 			global_position = attached_to.global_position + attach_offset
 			linear_velocity = Vector2.ZERO
-			return
-
-func _try_attach_on_drop() -> void:
-	if not attach_area or attached_to:
-		return
-	for body in attach_area.get_overlapping_bodies():
-		if body == self:
-			continue
-		if body is CharacterBody2D:
-			_attach_to(body)
-			return
 
 func _attach_to(body: CharacterBody2D) -> void:
 	attached_to = body
