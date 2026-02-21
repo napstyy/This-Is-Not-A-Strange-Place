4a8e5fb56f4804507979f3b1fe0860fae80edfa1
doubleTS <tylertsoberanis@gmail.com>
2026-02-21 04:03:18 -0600

    Made WordRoom text clearer

--- prefabs/Brickmove.gd
+++ prefabs/Brickmove.gd
@@ -3,6 +3,9 @@ extends StaticBody2D
 @onready var area: Area2D = $Area2D 
 @onready var guysprite: AnimatedSprite2D = $Guysprite
 @onready var cartsprite: AnimatedSprite2D = $Cartsprite
+@onready var spherewheel: Sprite2D = $Cartsprite/Sprite2D
+@onready var animation_player: AnimationPlayer = $Cartsprite/Sprite2D/AnimationPlayer
+
 
 var is_pushed: bool = false
 var push_speed: float = 25.0
@@ -21,4 +24,6 @@ func _on_body_entered(body):
 		guysprite.stop()
 		cartsprite.play("push")
 		is_pushed = true
+		spherewheel.set_visible(true)
+		animation_player.play("sphereroll")
 		GameManager.keys["Cart"] = true
